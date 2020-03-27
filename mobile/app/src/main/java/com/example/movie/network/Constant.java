1314b220a45d8e030f8deb81d65a50f112bc1b6c
aqillanovia <62745891+aqillanovia@users.noreply.github.com>
2020-03-27 17:11:51 +0700

    Add files via upload

---
+++ mobile/app/src/main/java/com/example/movie/network/Constant.java
@@ -0,0 +1,12 @@
+ package com.example.movie.network;
+
+public class Constant {
+    public static final String BASE_URL = "https://api.themoviedb.org";
+    public static final String IMG_URL = "https://image.tmdb.org/t/p/w500";
+    public static final String MOVIE_API_KEY = "3352d9d141c4a3cd9b44f05942b8e46e";
+    public static final String VERSION = "/3";
+    public static final String MOVIE = "/movie";
+    public static final String LANG_EN = "en-US";
+
+    public static final String MOVIE_PATH = VERSION+MOVIE;
+}
